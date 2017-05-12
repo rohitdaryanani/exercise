e8ff72e073eb1b8ce05e2a20572c16afc4ecd4ac
Rohit Daryanani <rohitdaryanani@yahoo.com>
2017-05-12 21:52:56 +0800

    refactor into single component

--- app.js
+++ app.js
@@ -1,64 +1,12 @@
 const Home = { template: '<div>Todo App Started.</div>' }
 
-const family = Vue.component('my-component', {
+const todo = Vue.component('my-component', {
     data() {
         return {
             todoItem: '',
-            todos: ['wash clothes', 'buy milk', 'sweep floor']
         }
     },
-    methods: {
-        addTodo(){
-            if(this.todoItem === '') return;
-            this.todos.push(this.todoItem);
-            this.todoItem = '';
-        },
-        removeTodo(index) {
-            this.todos.splice(index, 1);
-        }
-    },
-    template:
-        `<div>
-            <input type="text" v-model="todoItem" v-on:keyup.enter="addTodo" />
-            <ul v-for="(todo, index) in todos">
-                <li>{{todo}} <button v-on:click="removeTodo(index)">x</button></li>
-            </ul>
-        </div>`
-})
-
-const school = Vue.component('my-component', {
-    data() {
-        return {
-            todoItem: '',
-            todos: ['do homeworks', 'read books']
-        }
-    },
-    methods: {
-        addTodo(){
-            if(this.todoItem === '') return;
-            this.todos.push(this.todoItem);
-            this.todoItem = '';
-        },
-        removeTodo(index) {
-            this.todos.splice(index, 1);
-        }
-    },
-    template:
-        `<div>
-            <input type="text" v-model="todoItem" v-on:keyup.enter="addTodo" />
-            <ul v-for="(todo, index) in todos">
-                <li>{{todo}} <button v-on:click="removeTodo(index)">x</button></li>
-            </ul>
-        </div>`
-})
-
-const exercise = Vue.component('my-component', {
-    data() {
-        return {
-            todoItem: '',
-            todos: ['go to the gym', 'lift some weights', 'drink protein shake']
-        }
-    },
+    props: ['todos'],
     methods: {
         addTodo(){
             if(this.todoItem === '') return;
@@ -81,9 +29,9 @@ const exercise = Vue.component('my-component', {
 
 const routes = [
   { path: '/home', component: Home },
-  { path: '/family', component: family },
-  { path: '/school', component: school },
-  { path: '/exercise', component: exercise }
+  { path: '/family', component: todo, props:{ todos: ['wash clothes','buy milk','sweep floor'] } },
+  { path: '/school', component: todo, props:{ todos: ['do homework','read books'] } },
+  { path: '/exercise', component: todo, props:{ todos: ['lift weights','drink protein shake'] } }
 ]
 
 const router = new VueRouter({
