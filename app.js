c485f5ffbab89029b9169f52bff3dfb48fc62317
Rohit Daryanani <rohitdaryanani@yahoo.com>
2017-05-12 15:14:34 +0800

    family component

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const Home = { template: '<div>Todo App Started.</div>' }
 
-const mycomponent = Vue.component('my-component', {
+const family = Vue.component('my-component', {
     data() {
         return {
+            todoItem: '',
             todos: ['wash clothes', 'buy milk', 'sweep floor']
         }
     },
-    template: '<div><ul v-for="todo in todos"><li>{{todo}}</li></ul></div>'
+    methods: {
+        addTodo(){
+            if(this.todoItem === '') return;
+            this.todos.push(this.todoItem);
+            this.todoItem = '';
+        },
+        removeTodo(index) {
+            this.todos.splice(index, 1);
+        }
+    },
+    template:
+        `<div>
+            <input type="text" v-model="todoItem" v-on:keyup.enter="addTodo" />
+            <ul v-for="(todo, index) in todos">
+                <li>{{todo}} <button v-on:click="removeTodo(index)">x</button></li>
+            </ul>
+        </div>`
 })
 
 
 const routes = [
   { path: '/home', component: Home },
-  { path: '/my', component: mycomponent }
+  { path: '/family', component: family }
 ]
 
 const router = new VueRouter({
